4fa48c5625f05d929ac5c785fe78d183057f50b4
jrs <hello@jrs.dev>
2023-10-03 14:22:55 -0700

    creating new variables

---
+++ Scripting Examples p5/Assets/Scripts/Varaiables.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Varaiables : MonoBehaviour
+{
+    // Public Global/Member Variables
+    public int Score = 0; // declare a whole number
+    public float ItemAmount = 19.95f; // declare a decimal number
+    public string PlayerName = "John Doe"; // declare a text
+    public bool IsPlayerActive = true; // declare a true/false
+
+    // Private Global/Member Variables
+    private int _collectibleAmount = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
