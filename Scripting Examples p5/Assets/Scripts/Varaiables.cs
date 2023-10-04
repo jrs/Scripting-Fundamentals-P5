8b84ceb733825c9b9a9c56661ef4f9abc6a2e3c7
jrs <hello@jrs.dev>
2023-10-04 13:12:46 -0700

    Update Variable file

--- Scripting Examples p5/Assets/Scripts/Varaiables.cs
+++ Scripting Examples p5/Assets/Scripts/Varaiables.cs
@@ -12,16 +12,29 @@ public class Varaiables : MonoBehaviour
 
     // Private Global/Member Variables
     private int _collectibleAmount = 0;
+    private float _horizontalInput = 0f;
+    private string _userName = "John007";
+    private bool _isGameOver = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        // make sure Score Variable is = 0
+        Score = 0; // assigning a Global/Member variable
         
+        // declare a local variable to track Start Position
+        Vector3 startPosition = transform.position;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // trying to update a local variables value
+        // we can not update a local variable
+        //startPosition = new Vector3(Vector3.right);
+
+        // create a variable to determine object key movement
+        float verticalInput = Input.GetAxis("Vertical");
     }
 }
